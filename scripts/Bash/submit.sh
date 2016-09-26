c5d30c89031f4cf266aefcb0d4eb489d06ca0083
Tommaso93 <tommaso.diotalevi@gmail.com>
2016-09-26 17:39:21 +0200

    Update submit.sh

--- scripts/Bash/submit.sh
+++ scripts/Bash/submit.sh
@@ -1,3 +1,6 @@
+# submit.sh Bash script
+# Tommaso Diotalevi
+# This script is used to start the execution of jsonUtilities.py script. Useful for batch jobs.
 #!/bin/bash
 
 cd /afs/cern.ch/work/t/tdiotale/test_2016/
@@ -5,4 +8,5 @@ cd /afs/cern.ch/work/t/tdiotale/test_2016/
 export PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/bin:$PATH
 export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/lib:$LD_LIBRARY_PATH
 
+# How to correctly execute jsonUtilities.py script
 python jsonUtilities.py --ifile dashb-wdt-251-data.1467324000030 --ohfile valid.csv
